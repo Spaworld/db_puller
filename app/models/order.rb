9ee19a1f6de0bf5723e7dfa0c254a0c0c7e89a9c
dimanyc <dimitrynazarov@gmail.com>
2016-09-02 19:42:59 -0400

    refines Order.default_scope to limit records by price and order type

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,10 +7,15 @@ class Order < RemoteDB
   alias_attribute :created_at,  ENV['ORDERS_DATE']
   alias_attribute :type,        ENV['ORDERS_TYPE']
   alias_attribute :channel_id,  ENV['ORDERS_CUSTOMER_ID']
+  alias_attribute :return?,     ENV['ORDERS_RETURN']
+  alias_attribute :total,       ENV['ORDERS_TOTAL']
 
   default_scope { where(
     channel_id: Channel.ids,
-    created_at: Time.now - 3.months...Time.now )}
+    created_at: Time.now - 3.months...Time.now,
+    return?:    false,
+    total:      (100..Float::INFINITY)
+  )}
 
   belongs_to :channel, foreign_key: ENV['ORDERS_CUSTOMER_ID']
 
