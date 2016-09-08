1a7d32278285a3157b84465ec2f2c56ea756144a
dimanyc <dimitrynazarov@gmail.com>
2016-09-08 13:30:00 -0400

    adds sku alias to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < RemoteDB
   self.table_name   = ENV['PRODUCTS_TABLE_NAME']
   self.primary_key  = ENV['PRODUCTS_PRIMARY_KEY']
 
+  alias_attribute :sku, ENV['PRODUCTS_SKU']
+
   has_many :order_details, foreign_key: ENV['ORDER_DETAIL_PRODUCT_ID']
 
 end
